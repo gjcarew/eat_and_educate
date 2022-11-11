55ab2b90f5532657e8b6be45448ee2713a0db146
Gavin Carew <gjcarew@gmail.com>
2022-11-10 22:36:55 -0800

    Recipe service

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,21 @@
+class RecipeService
+  def self.search(query)
+    query_params = {
+      type: 'public',
+      app_id: ENV['RECIPE_ID'],
+      app_key: ENV['RECIPE_KEY'],
+      q: query,
+      field: %w[label image url]
+    }
+    response = conn.get('api/recipes/v2', params)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn 
+    Faraday.new(url: 'https://api.edamam.com') do |faraday|
+      faraday.options.params_encoder = Faraday::FlatParamsEncoder
+    end
+  end
+end
---
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes API' do 
+  it 'gets a list of recipes from a certain country'
+    # create_list(:recipe, 3, country: 'thailand')
+    
+end
