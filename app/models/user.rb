f79f3462139e6c1ad0aff8086fbfdeada7eb00f7
Gavin Carew <gjcarew@gmail.com>
2022-11-13 23:05:04 -0800

    User registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, :email, :password_digest, presence: true
+  validates :name, :email, :api_key, presence: true
   validates_uniqueness_of :email, { case_sensitive: false }
   validates_uniqueness_of :api_key
   has_secure_password
-  before_save { self.email = email.downcase }
+
 end
