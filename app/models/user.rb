20e8f3a93e6600d57c172d1b2912579cf6689284
Gavin Carew <gjcarew@gmail.com>
2022-11-14 14:43:26 -0800

    Move API key creation from UsersController$create to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  before_validation :set_api_key
   validates :name, :email, :api_key, presence: true
   validates_uniqueness_of :email, { case_sensitive: false }
   validates_uniqueness_of :api_key
   has_secure_password
 
+  private
+
+    def set_api_key
+      self.api_key = SecureRandom.hex if self.api_key.nil?
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:api_key) }
     it { should validate_uniqueness_of(:email).case_insensitive.on(:create) }
     it { should validate_uniqueness_of(:api_key).on(:create) }
     it { should have_secure_password }
