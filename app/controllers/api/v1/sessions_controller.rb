7198db16af495486d959a530a766cae144d578e1
Gavin Carew <gjcarew@gmail.com>
2022-11-15 23:42:17 -0800

    Weird file corruption, had to copy from old branches to fix

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,16 +2,18 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email].downcase)
+    return render json: { "error": "No user with that email" }, status: :not_found if user.nil?
+
     if user&.authenticate(params[:password])
       log_in(user)
       render json: UserSerializer.new(user), status: :created
     else 
-      render json: user.errors, status: :unprocessable_entity
+      render json: { "error": "Incorrect password" }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    log_out
+    session.delete(:user_id)
   end
 
   private
@@ -21,8 +23,4 @@ class Api::V1::SessionsController < ApplicationController
     session[:user_id] = user.id
   end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
 end
